a21295b046fa93f1db880693efe7557678228e80
Junjie <junjie.eric.guo@gmail.com>
2016-03-14 14:55:01 +1100

    Update MySQL and Socket configuration

--- resources/views/receive.blade.php
+++ resources/views/receive.blade.php
@@ -8,8 +8,8 @@
 @section('footer')
 
     <script>
-      //  var socket = io('http://localhost:3000'); //local environment
-          var socket = io('http://52.62.75.213:3000'); //product environment
+        var socket = io('http://localhost:3000'); //local environment
+        //  var socket = io('http://52.62.75.213:3000'); //product environment
         $('form').submit(function(){
 
             var msg = $('#m').val();
